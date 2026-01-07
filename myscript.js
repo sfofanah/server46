fa393dc90948724c10d826bb3c57e5dde4c6b0e1
sfofanah <sfofanah4@gmail.com>
2026-01-07 11:44:39 -0500

    debugging my JS

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ const myClanCards = document.querySelectorAll(".new-card-link");
 console.log("Clan links found:", myClanCards.length);
 
 myClanCards.forEach(function(card) {
-    card.addEventListener("click", function() {
-        console.log("A clan card was clicked");
-    });
+  card.addEventListener("click", function() {
+    console.log("A clan card was clicked");
+  });
 });
