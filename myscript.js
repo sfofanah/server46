9f5eafe8e74f4567149d48b09b8cb9d607a78d44
sfofanah <sfofanah4@gmail.com>
2026-01-07 12:20:05 -0500

    updating my JS

--- myscript.js
+++ myscript.js
@@ -4,7 +4,11 @@ console.log("Clan links found:", myClanCards.length);
 myClanCards.forEach(function(card) {
   card.addEventListener("click", function(e) {
     e.preventDefault(); // stops the page from navigating away
-    console.log("A clan card was clicked", e.target);
+
+    const clickedCard = e.target;
+    const clanName = clickedCard.dataset.clan;
+
+    console.log("Clan clicked:", clanName);
   });
 });
 
