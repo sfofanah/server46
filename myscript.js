510412ad58b6512b22124c275ac3df9ab018576c
sfofanah <sfofanah4@gmail.com>
2026-01-07 11:40:45 -0500

    implementing more JavaScript

--- myscript.js
+++ myscript.js
@@ -1,2 +1,8 @@
 const myClanCards = document.querySelectorAll(".new-card-link");
 console.log("Clan links found:", myClanCards.length);
+
+myClanCards.forEach(function(card) {
+    card.addEventListener("click", function() {
+        console.log("A clan card was clicked");
+    });
+});
