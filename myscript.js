3df1b9d23a4237ea1e88671988740c6291ff6128
sfofanah <sfofanah4@gmail.com>
2026-02-25 14:53:33 -0500

    Learning Functions in JS

--- myscript.js
+++ myscript.js
@@ -94,50 +94,9 @@ nameBtn.addEventListener("click", function() {
   
 });
 
+/* ----------------------------- CLS.html (Learning Functions) ------------------------------------------ */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-myTest.addEventListener("click", function(event) {
-    event.preventDefault();
-    myTest.textContent = "I changed the text using JS";
-})
-
-*/
-
-
-
-
-/*
-
-const myClanCards = document.querySelectorAll(".new-card-link");
-console.log("Clan links found:", myClanCards.length);
-
-myClanCards.forEach(function(card) {
-  card.addEventListener("click", function(e) {
-    e.preventDefault(); // stops the page from navigating away
-
-    const clickedCard = e.currentTarget;
-    const clanName = clickedCard.dataset.clan;
-
-    console.log("Clan clicked:", clanName);
-  });
-});
-
-*/
+function greetLeader(name) {
+  console.log("welcome, " + name);
+}
+greetLeader("Rick");
